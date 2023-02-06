2a460545586cfd35477e312e54c96a9fb89eb048
kanamonra <ceceg@inha.edu>
2023-02-06 09:39:00 +0900

    second commit toString

---
+++ DAY16/src/Node.java
@@ -0,0 +1,10 @@
+public class Node {
+    public String data;
+    public Node link;
+    @Override
+    public String toString() {
+        return "Node data resource is " + data;
+    }
+
+
+}
---
+++ DAY16/src/day16.java
@@ -0,0 +1,11 @@
+
+public class day16
+{
+    public static void main(String[] args) {
+        Node node = new Node();
+        node.data = "Pikachu";
+        System.out.println(node.toString());
+
+
+    }
+}
