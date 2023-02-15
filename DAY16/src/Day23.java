717b54dc170de9621bd7f279e808ff1d10c2cd2a
kanamonra <ceceg@inha.edu>
2023-02-15 13:35:22 +0900

    day23 network interface v2

--- DAY16/src/Day23.java
+++ DAY16/src/Day23.java
@@ -1,20 +1,25 @@
+import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Collections;
 import java.util.Enumeration;
 
-// check network
+// check network address
 // Java network interface -> test for get Inet address, name, display, interface
 public class Day23 {
     public static void main(String[] args) {
         try {
             Enumeration<NetworkInterface> interfaceEnum = NetworkInterface.getNetworkInterfaces();
-            System.out.print("Name         Display name\n");
+            System.out.printf("Name Display name\n");
             for (NetworkInterface element : Collections.list(interfaceEnum)) {
                 System.out.printf("%-8s %-32s\n", element.getName(), element.getDisplayName());
-            }  // %-8s name display name space
+                Enumeration<InetAddress> addresses = element.getInetAddresses();
+                for (InetAddress inetAddress : Collections.list(addresses)) {
+                    System.out.printf("InetAddress: %s\n", inetAddress);
+                }
+            }
         } catch (SocketException e) {
-            System.out.println("Socket exception error");
+            throw new RuntimeException(e);
         }
     }
 }
